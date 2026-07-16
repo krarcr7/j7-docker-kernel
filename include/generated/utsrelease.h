85867763c305250986a8948675b4197997aa5e7e
krarcr7 <smart.633219@gmail.com>
2026-07-17 02:39:25 +0300

    Fix build config and docker kernel options

---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.18.91-gcb3d3bb8-dirty"
---
+++ scripts/mod/.devicetable-offsets.s.d
@@ -0,0 +1,24 @@
+scripts/mod/devicetable-offsets.s: scripts/mod/devicetable-offsets.c \
+  include/linux/kconfig.h include/generated/autoconf.h \
+  include/linux/kbuild.h include/linux/mod_devicetable.h \
+  include/linux/types.h include/uapi/linux/types.h \
+  arch/arm64/include/generated/asm/types.h \
+  include/uapi/asm-generic/types.h include/asm-generic/int-ll64.h \
+  include/uapi/asm-generic/int-ll64.h \
+  arch/arm64/include/uapi/asm/bitsperlong.h \
+  include/asm-generic/bitsperlong.h \
+  include/uapi/asm-generic/bitsperlong.h \
+  include/uapi/linux/posix_types.h include/linux/stddef.h \
+  include/uapi/linux/stddef.h include/linux/compiler.h \
+  include/linux/compiler-gcc.h include/linux/compiler-clang.h \
+  arch/arm64/include/uapi/asm/posix_types.h \
+  include/uapi/asm-generic/posix_types.h include/linux/uuid.h \
+  include/uapi/linux/uuid.h include/linux/string.h \
+  /data/data/com.termux/files/usr/lib/clang/21/include/stdarg.h \
+  /data/data/com.termux/files/usr/lib/clang/21/include/__stdarg_header_macro.h \
+  /data/data/com.termux/files/usr/lib/clang/21/include/__stdarg___gnuc_va_list.h \
+  /data/data/com.termux/files/usr/lib/clang/21/include/__stdarg_va_list.h \
+  /data/data/com.termux/files/usr/lib/clang/21/include/__stdarg_va_arg.h \
+  /data/data/com.termux/files/usr/lib/clang/21/include/__stdarg___va_copy.h \
+  /data/data/com.termux/files/usr/lib/clang/21/include/__stdarg_va_copy.h \
+  include/uapi/linux/string.h arch/arm64/include/asm/string.h
